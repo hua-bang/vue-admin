2b3861b7b09398ea73e24bee0cce2482411ae637
hua-bang <1964760211@qq.com>
2020-08-05 19:46:46 +0800

    构建项目，配置全局样式，路由,element-ui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="login">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+};
+</script>
+
+<style lang="scss" scoped>
+  #login{
+    height: 100vh;
+    background-color: #344a5f;
+  }
+</style>
