116f9ff3e6f23d1692c5b45b06106acde770265f
hua-bang <1964760211@qq.com>
2020-08-05 21:24:20 +0800

    登录页面的表单基本完工，后端还没接入

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,12 +1,114 @@
 <template>
   <div id="login">
+    <div class="login-wrap">
+      <ul class="menu-tab">
+        <li v-for="item in menuTab" :key="item.id" :class="{'current':item.current}" @click="toggleMenu(item)">{{item.txt}}</li>
+      </ul>
+      <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" class="login-form" size="medium">
+        <el-form-item prop="username" class="item-form">
+          <label>邮箱</label>
+          <el-input v-model="ruleForm.username" autocomplete="off"></el-input>
+        </el-form-item>
 
+        <el-form-item prop="password" class="item-form">
+          <label>密码</label>
+          <el-input type="password" v-model="ruleForm.password" autocomplete="off" minlength="6" maxlength="20"></el-input>
+        </el-form-item>
+
+        <el-form-item prop="code" class="item-form">
+          <label>验证码</label>
+          <el-row :gutter="10">
+            <el-col :span="15">
+              <el-input v-model.number="ruleForm.code" minlength="6" maxlength="6"></el-input>
+            </el-col>
+            <el-col :span="9">
+              <el-button type="success" class="block">获取验证码</el-button>
+            </el-col>
+          </el-row>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="danger" @click="submitForm('ruleForm')" class="login-btn block">提交</el-button>
+        </el-form-item>
+
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "index"
+  name: "login",
+  data() {
+    let validateCode = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error('验证码不能为空'));
+      }else{
+        callback();
+      }
+    };
+    let validatePassword = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'));
+      } else {
+        if (this.ruleForm.checkPass !== '') {
+          this.$refs.ruleForm.validateField('checkPass');
+        }
+        callback();
+      }
+    };
+    let validateUsername = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入用户名'));
+      }else {
+        callback();
+      }
+    };
+    return {
+      menuTab: [
+        {txt: "登录", current: true},
+        {txt: "注册", current: false}
+      ],
+      ruleForm: {
+        username:'',
+        password: '',
+        code: ''
+      },
+      rules: {
+        username: [
+          { validator: validateUsername, trigger: 'blur' }
+        ],
+        password: [
+          { validator: validatePassword, trigger: 'blur' }
+        ],
+        code: [
+          { validator: validateCode, trigger: 'blur' }
+        ]
+      }
+    };
+  },
+  created() {
+  },
+  mounted() {
+  },
+  methods:{
+    toggleMenu(data){
+      this.menuTab.forEach(element => {
+        element.current = false
+      })
+      data.current = true
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!');
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+  }
 };
 </script>
 
@@ -14,5 +116,46 @@ export default {
   #login{
     height: 100vh;
     background-color: #344a5f;
+  }
+  .login-wrap{
+    width: 330px;
+    margin: auto;
+  }
+  .menu-tab{
+    text-align: center;
+    margin-block-start: 0;
+    margin-block-end: 0;
+    line-height: 36px;
+    padding-inline-start: 0;
+    li {
+      display: inline-block;
+      width: 88px;
+      font-size: 14px;
+      color: #fff;
+      border-radius: 2px;
+      margin: 0;
+      cursor: pointer;
+    }
+    .current{
+      background-color: rgba(0,0,0,.1);
+    }
+  }
+  .login-form{
+    label{
+      display: block;
+      margin-bottom: 3px;
+      font-size: 14px;
+      color: #fff;
+    }
+    .item-form{
+      margin-bottom: 10px;
+    }
+    .block{
+      display: block;
+      width: 100%;
+    }
+    .login-btn{
+      margin-top: 14px;
+    }
   }
 </style>
